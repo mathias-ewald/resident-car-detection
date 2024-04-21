afaf096d8390073dda990a8943835c421f3aa2bd
Mathias Ewald <mathias.ewald@googlemail.com>
2024-04-21 07:27:26 +0200

    update

---
+++ Our Cars.v2i.yolov8/README.dataset.txt
@@ -0,0 +1,6 @@
+# Our Cars > 2024-04-19 12:20pm
+https://universe.roboflow.com/private-workspace-0pk4a/our-cars
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
--- notebook.py
+++ notebook.py
@@ -8,9 +8,8 @@ app = marimo.App()
 def __():
     import marimo as mo
 
-    EPOCHS = 3
     TEST_PICTURE = '/Users/mewald/Desktop/NVR-Eingang 19-04-2024, 12-26-24.jpg'
-    return EPOCHS, TEST_PICTURE, mo
+    return TEST_PICTURE, mo
 
 
 @app.cell
@@ -54,24 +53,12 @@ def __():
 
 
 @app.cell
-def __(EPOCHS, model):
-    def train(data, settings = {}):
-        return model.train(
-            data=data,
-            epochs=EPOCHS, 
-            device='mps',
-            **settings
-        )
-    return train,
-
-
-@app.cell
 def __(model):
     import cv2
     from PIL import Image
 
     def infer(source, destination):
-        results = model.predict(source=source)
+        results = model(source)
         for r in results:
             im_bgr = r.plot() 
             im_rgb = Image.fromarray(im_bgr[..., ::-1]) 
@@ -81,31 +68,24 @@ def __(model):
 
 
 @app.cell
-def __(train):
-    coco128_results = train(data='coco128.yaml')
-    return coco128_results,
+def __(TEST_PICTURE, infer):
+    pretrained_result = infer(TEST_PICTURE, "pretrain-result.jpg")
+    return pretrained_result,
+
+
+@app.cell
+def __(model):
+    ourcars_train_results = model.train(
+        data='./Our Cars.v2i.yolov8/data.yaml',
+        epochs=100
+    )
+    return ourcars_train_results,
 
 
 @app.cell
 def __(TEST_PICTURE, infer):
-    before_result = infer(TEST_PICTURE, "before-train.jpg")
-    for key in before_result.names:
-        print(before_result.names[key])
-    return before_result, key
-
-
-@app.cell
-def __(train):
-    ourcars_results = train(data='./Our Cars.v2i.yolov8/data.yaml')
+    ourcars_results = infer(TEST_PICTURE, "ourcars-result.jpg")
     return ourcars_results,
-
-
-@app.cell
-def __(TEST_PICTURE, infer):
-    after_result = infer(TEST_PICTURE, "after-train.jpg")
-    for k in after_result.names:
-        print(after_result.names[k])
-    return after_result, k
 
 
 if __name__ == "__main__":
