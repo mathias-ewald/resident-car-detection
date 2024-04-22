68c238e1a169fcf807da5ccc843fbd91d6bafbad
Mathias Ewald <mathias.ewald@googlemail.com>
2024-04-22 07:38:32 +0200

    update

--- Our Cars.v2i.yolov8/README.dataset.txt
+++
@@ -1,6 +0,0 @@
-# Our Cars > 2024-04-19 12:20pm
-https://universe.roboflow.com/private-workspace-0pk4a/our-cars
-
-Provided by a Roboflow user
-License: CC BY 4.0
-
--- notebook.py
+++ notebook.py
@@ -5,23 +5,26 @@ app = marimo.App()
 
 
 @app.cell
-def __():
+def __(__file__):
     import marimo as mo
+    import os
 
-    TEST_PICTURE = '/Users/mewald/Desktop/NVR-Eingang 19-04-2024, 12-26-24.jpg'
-    return TEST_PICTURE, mo
+    MODEL_VERSION = "yolov8x.pt"
+    TEST_PICTURE = 'test.jpg'
+    BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+    return BASE_DIR, MODEL_VERSION, TEST_PICTURE, mo, os
 
 
 @app.cell(hide_code=True)
-def __(mo):
+def __(MODEL_VERSION, mo):
     mo.md(f"""
-    # Pretrained Model
+    # Pretrained Model: {MODEL_VERSION}
     """)
     return
 
 
 @app.cell
-def __():
+def __(MODEL_VERSION):
     from ultralytics import YOLO
     from ultralytics import settings
 
@@ -30,7 +33,7 @@ def __():
         'runs_dir': './runs',     
     })
 
-    model = YOLO('yolov8x.pt')
+    model = YOLO(MODEL_VERSION)
     return YOLO, model, settings
 
 
@@ -51,8 +54,8 @@ def __(model):
 
 @app.cell
 def __(TEST_PICTURE, infer, mo):
-    pretrained_result = infer(TEST_PICTURE, "pretrain-result.jpg")
-    mo.image(src="pretrain-result.jpg")
+    pretrained_result = infer(TEST_PICTURE, "tmp/pretrain-result.jpg")
+    mo.image(src="tmp/pretrain-result.jpg")
     return pretrained_result,
 
 
@@ -65,7 +68,7 @@ def __(mo):
 
 
 @app.cell
-def __(model):
+def __(BASE_DIR, model, os):
     import torch
     import torch.backends.mps as mps
 
@@ -73,18 +76,21 @@ def __(model):
     device = torch.device(device_str)
 
     ourcars_train_results = model.train(
-        data='./Our Cars.v2i.yolov8/data.yaml',
+        data=os.path.join(BASE_DIR, 'datasets/ourcars/data.yaml'),
         epochs=100,
         device=device,
         plots=True,
+
+        degrees=20,
+        translate=0.3
     )
     return device, device_str, mps, ourcars_train_results, torch
 
 
 @app.cell
 def __(TEST_PICTURE, infer, mo):
-    ourcars_results = infer(TEST_PICTURE, "ourcars-result.jpg")
-    mo.image(src="ourcars-result.jpg")
+    ourcars_results = infer(TEST_PICTURE, "tmp/ourcars-result.jpg")
+    mo.image(src="tmp/ourcars-result.jpg")
     return ourcars_results,
 
 
